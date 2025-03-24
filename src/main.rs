b16abace63a9a58599bfc739d504ef714e29226f
Samuel Beauzile <prodigybatman32146@gmail.com>
2025-03-24 11:48:48 -0400

    We making changes. I wpnt add the things that i compiled to this though

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,36 @@ use serde_json::{json, Value};
 
 
 
-fn main() {
-    
+
+// Here is where I think i will be setting up the request I would be getting from the internet
+#[derive(Deserialize, Debug)]
+struct Request 
+{
+    name: String,
+}
+
+#[derive(Serialize, Debug)]
+struct Response 
+{
+    message: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> 
+{
+    lambda_runtime::run(service_fn(faas_practice)).await?;
+    Ok(())
 }
 
 // This is a function that returs nothing -> is for return type
 
-fn faas_practice() -> ()
+async fn faas_practice(event: LambdaEvent<Request>) -> Result<Value, Error>
 {
-    
+    let name = event.payload.name;
+    let message = format!("Hello, {}!", name);
+    let resp = Response { message };
+
+    Ok(json!(resp))
 }
+
+
