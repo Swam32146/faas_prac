811cc1bd929976bc7247ce9c7deb3e51d17670de
Samuel Beauzile <prodigybatman32146@gmail.com>
2025-03-24 10:15:47 -0400

    This is my FaaS application I will be practicing writing

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+
+
+fn main() {
+    
+}
+
+// This is a function that returs nothing -> is for return type
+
+fn faas_practice() -> ()
+{
+    
+}
